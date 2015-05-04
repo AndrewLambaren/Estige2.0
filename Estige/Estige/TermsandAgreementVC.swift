3723fdacc2e5cac3e8cb2d92c90d0288dc02b3fc
AndrewLambaren <jesus@Jesuss-MBP.westell.com>
2015-05-04 16:22:01 -0500

    Added a Policy Page
    Policy Page is replacing the help page for the moment

--- Estige/Estige/TermsandAgreementVC.swift
+++ Estige/Estige/TermsandAgreementVC.swift
@@ -10,26 +10,51 @@ import UIKit
 
 class TermsandAgreementVC: UIViewController {
 
+    @IBAction func btnBack(sender: UIBarButtonItem) {
+        self.dismissViewControllerAnimated(false, completion: nil)
+    }
+    
+    @IBOutlet weak var webview: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        LoadLocalPDF()
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    func LoadLocalPDF()
+    {
+        webview.loadLocalPDF("PrivacyPolicy.pdf")
+        //webview.loadExternalPDF("URL TO PDF")
+    }
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
+}
+extension UIWebView {
+    func loadLocalPDF(name:String!) {
+        //load local pdf
+        let termsPath:String? = NSBundle.mainBundle().pathForResource(name, ofType: "pdf")!
+        let url = NSURL(fileURLWithPath: termsPath!)
+        let pdfRequest = NSURLRequest(URL: url!)
+        self.loadRequest(pdfRequest)
+    }
+    func loadExternalPDF(name:String!){
+        let url = NSURL(string: name)
+        let request = NSURLRequest(URL:url!)
+        self.scalesPageToFit = true
+        self.loadRequest(request)
+    }
 }
